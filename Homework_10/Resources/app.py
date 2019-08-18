e7da856408233f85d22b29431c88852a9a5e336a
Mike Walsh <dabc@cox.net>
2019-08-17 21:04:10 -0400

    added app.py

---
+++ Homework_10/Resources/app.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import requests
+import time
+import sys
+import json
+import csv
+import datetime
+from datetime import timedelta
+from datetime import datetime
+from dateutil.parser import parse
+
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Welcome to Climate App!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/station<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
+
+@app.route("/api/v1.0/station")
+def station():
+    """Return a JSON list of stations from the dataset"""
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(Station.station, Station.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+
+    return jsonify(results)
+
+
+
+
